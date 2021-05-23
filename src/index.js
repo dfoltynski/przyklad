3902515eb55553ca1990d4a438d4962dc90d0058
dfoltynski <dawidfoltynski7@gmail.com>
2021-05-23 12:13:40 +0200

    validate email and phone number

--- src/index.js
+++ src/index.js
@@ -34,17 +34,61 @@ uniqueCode.textContent += randomUniqueCode;
 const form = document.querySelector("form");
 
 const showErrorMessage = (node, message) => {
-  const errorMessage = document.querySelectorAll("small")[0];
+  const errorMessage = node.nextSibling.nextElementSibling;
   errorMessage.textContent = message;
   errorMessage.classList.add("input__error--message");
   node.classList.add("input__error");
 };
 
 const resetErrorMessage = (node) => {
-  const errorMessage = document.querySelectorAll("small")[0];
+  const errorMessage = node.nextSibling.nextElementSibling;
   errorMessage.textContent = "";
   errorMessage.classList.remove("input__error--message");
   node.classList.remove("input__error");
+};
+
+const validateEmail = (email) => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+const validatePhoneNumber = (ev, node) => {
+  const phone = ev.path[0] || node;
+  const phoneNo = phone.value.trim();
+
+  // const errorMessage = phone.nextSibling.nextElementSibling;
+  if (isNaN(phoneNo)) {
+    showErrorMessage(phone, "Podaj poprawny numer telefonu");
+  } else {
+    if (phoneNo.length == 9) {
+      resetErrorMessage(phone);
+      return true;
+    } else {
+      showErrorMessage(phone, "Numer telefonu jest za krótki");
+    }
+  }
+};
+
+const validatePostCode = (ev, node) => {
+  const postCode = ev.path[0] || node;
+  const postCodeNo = postCode.value.trim();
+
+  if (postCodeNo.length == 2) {
+    postCode.value += postCodeNo + "-";
+  }
+
+  console.log(postCodeNo);
+  // if (isNaN(postCodeNo)) {
+  //   showErrorMessage(postCode, "Podaj poprawny kod pocztowy");
+  // } else {
+  //   if (postCodeNo.length == 5) {
+  //     resetErrorMessage(postCode);
+  //     return true;
+  //   } else {
+  //     showErrorMessage(postCode, "Kod pocztowy jest nieprawidłowy");
+  //   }
+  // }
 };
 
 const validate = (name, email, phone, street, city, postcode) => {
@@ -53,7 +97,51 @@ const validate = (name, email, phone, street, city, postcode) => {
   } else {
     resetErrorMessage(name);
   }
+
+  if (email.value.trim() == "") {
+    showErrorMessage(email, "Podaj e-mail");
+  } else {
+    if (validateEmail(email.value.trim())) {
+      resetErrorMessage(email);
+    } else {
+      showErrorMessage(email, "Adres e-mail jest niepoprawny");
+    }
+  }
+
+  if (phone.value.trim() == "") {
+    showErrorMessage(phone, "Podaj numer telefonu");
+  } else {
+    if (validatePhoneNumber(phone)) {
+      resetErrorMessage(phone);
+    }
+  }
+
+  if (street.value.trim() == "") {
+    showErrorMessage(street, "Podaj ulice");
+  } else {
+    resetErrorMessage(street);
+  }
+
+  if (city.value.trim() == "") {
+    showErrorMessage(city, "Podaj miasto");
+  } else {
+    resetErrorMessage(city);
+  }
+
+  if (postcode.value.trim() == "") {
+    showErrorMessage(postcode, "Podaj kod pocztowy");
+  } else {
+    resetErrorMessage(postcode);
+  }
 };
+
+document
+  .querySelector(".input__personal--phone")
+  .addEventListener("keyup", validatePhoneNumber);
+
+document
+  .querySelector(".input__delivery--postcode")
+  .addEventListener("keyup", validatePostCode);
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -65,7 +153,7 @@ form.addEventListener("submit", (e) => {
   const street = document.querySelector(".input__delivery--street");
   const city = document.querySelector(".input__delivery--city");
   const postcode = document.querySelector(".input__delivery--postcode");
-  console.log(name);
+
   const result = validate(name, email, phone, street, city, postcode);
 });
 // HANDLE SUBMIT
