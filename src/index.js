ee1013c531e35e7b30eaaf219ee5a9b5402265ea
dfoltynski <dawidfoltynski7@gmail.com>
2021-05-23 13:15:43 +0200

    done v1 - validate all data and display them in console

--- src/index.js
+++ src/index.js
@@ -33,6 +33,8 @@ uniqueCode.textContent += randomUniqueCode;
 // HANDLE SUBMIT
 const form = document.querySelector("form");
 
+const rodo = document.querySelector(".rodo__checkbox");
+
 const showErrorMessage = (node, message) => {
   const errorMessage = node.nextSibling.nextElementSibling;
   errorMessage.textContent = message;
@@ -43,27 +45,28 @@ const showErrorMessage = (node, message) => {
 const resetErrorMessage = (node) => {
   const errorMessage = node.nextSibling.nextElementSibling;
   errorMessage.textContent = "";
-  errorMessage.classList.remove("input__error--message");
   node.classList.remove("input__error");
 };
 
-const validateEmail = (email) => {
+const validateEmail = (ev, node) => {
+  const email = node || ev.path[0];
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(String(email.value.trim()).toLowerCase())) {
+    resetErrorMessage(email);
+  }
+  return re.test(String(email.value.trim()).toLowerCase());
 };
 
 const validatePhoneNumber = (ev, node) => {
-  const phone = ev.path[0] || node;
+  const phone = node || ev.path[0];
   const phoneNo = phone.value.trim();
 
-  // const errorMessage = phone.nextSibling.nextElementSibling;
   if (isNaN(phoneNo)) {
     showErrorMessage(phone, "Podaj poprawny numer telefonu");
   } else {
     if (phoneNo.length == 9) {
       resetErrorMessage(phone);
-      return true;
     } else {
       showErrorMessage(phone, "Numer telefonu jest za krótki");
     }
@@ -71,24 +74,23 @@ const validatePhoneNumber = (ev, node) => {
 };
 
 const validatePostCode = (ev, node) => {
-  const postCode = ev.path[0] || node;
+  const postCode = node || ev.path[0];
   const postCodeNo = postCode.value.trim();
 
   if (postCodeNo.length == 2) {
-    postCode.value += postCodeNo + "-";
+    postCode.value += "-";
   }
 
-  console.log(postCodeNo);
-  // if (isNaN(postCodeNo)) {
-  //   showErrorMessage(postCode, "Podaj poprawny kod pocztowy");
-  // } else {
-  //   if (postCodeNo.length == 5) {
-  //     resetErrorMessage(postCode);
-  //     return true;
-  //   } else {
-  //     showErrorMessage(postCode, "Kod pocztowy jest nieprawidłowy");
-  //   }
-  // }
+  if (isNaN(postCodeNo.split("-").join(""))) {
+    showErrorMessage(postCode, "Podaj poprawny kod pocztowy");
+  } else {
+    if (postCodeNo.length == 6) {
+      resetErrorMessage(postCode);
+      return true;
+    } else {
+      showErrorMessage(postCode, "Kod pocztowy jest nieprawidłowy");
+    }
+  }
 };
 
 const validate = (name, email, phone, street, city, postcode) => {
@@ -101,7 +103,7 @@ const validate = (name, email, phone, street, city, postcode) => {
   if (email.value.trim() == "") {
     showErrorMessage(email, "Podaj e-mail");
   } else {
-    if (validateEmail(email.value.trim())) {
+    if (validateEmail(undefined, email)) {
       resetErrorMessage(email);
     } else {
       showErrorMessage(email, "Adres e-mail jest niepoprawny");
@@ -111,7 +113,7 @@ const validate = (name, email, phone, street, city, postcode) => {
   if (phone.value.trim() == "") {
     showErrorMessage(phone, "Podaj numer telefonu");
   } else {
-    if (validatePhoneNumber(phone)) {
+    if (validatePhoneNumber(undefined, phone)) {
       resetErrorMessage(phone);
     }
   }
@@ -131,29 +133,72 @@ const validate = (name, email, phone, street, city, postcode) => {
   if (postcode.value.trim() == "") {
     showErrorMessage(postcode, "Podaj kod pocztowy");
   } else {
-    resetErrorMessage(postcode);
+    if (validatePostCode(undefined, postcode)) resetErrorMessage(postcode);
   }
+
+  return true;
 };
 
-document
-  .querySelector(".input__personal--phone")
-  .addEventListener("keyup", validatePhoneNumber);
+const name = document.querySelector(".input__personal--name");
+name.addEventListener("keyup", (ev) => {
+  const name = ev.path[0];
+  if (name.value.trim() == "") {
+    showErrorMessage(name, "Podaj imię i nazwisko");
+  } else {
+    resetErrorMessage(name);
+  }
+});
 
-document
-  .querySelector(".input__delivery--postcode")
-  .addEventListener("keyup", validatePostCode);
+const email = document.querySelector(".input__personal--email");
+email.addEventListener("keyup", validateEmail);
+
+const phone = document.querySelector(".input__personal--phone");
+phone.addEventListener("keyup", validatePhoneNumber);
+
+const street = document.querySelector(".input__delivery--street");
+street.addEventListener("keyup", (ev) => {
+  const street = ev.path[0];
+
+  if (street.value.trim() == "") {
+    showErrorMessage(street, "Podaj ulice");
+  } else {
+    resetErrorMessage(street);
+  }
+});
+
+const city = document.querySelector(".input__delivery--city");
+city.addEventListener("keyup", (ev) => {
+  const city = ev.path[0];
+
+  if (city.value.trim() == "") {
+    showErrorMessage(city, "Podaj miasto");
+  } else {
+    resetErrorMessage(city);
+  }
+});
+
+const postcode = document.querySelector(".input__delivery--postcode");
+postcode.addEventListener("keyup", validatePostCode);
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const name = document.querySelector(".input__personal--name");
-  const email = document.querySelector(".input__personal--email");
-  const phone = document.querySelector(".input__personal--phone");
+  validate(name, email, phone, street, city, postcode);
 
-  const street = document.querySelector(".input__delivery--street");
-  const city = document.querySelector(".input__delivery--city");
-  const postcode = document.querySelector(".input__delivery--postcode");
+  const errors = document.querySelectorAll(".input__error");
+  const selectedAmount = document.querySelector(".price--active").childNodes[0];
 
-  const result = validate(name, email, phone, street, city, postcode);
+  if (errors.length == 0 && rodo.checked) {
+    console.log(
+      name.value,
+      email.value,
+      phone.value,
+      street.value,
+      city.value,
+      postcode.value,
+      selectedAmount
+    );
+  }
 });
+
 // HANDLE SUBMIT
